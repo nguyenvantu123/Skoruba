8b55663af61e6503cc925e41cb04585234eb9b11
nguyenvantu <nguyenvantu020794@gmail.com>
2026-05-13 18:33:45 +0700

    commit

--- src/Skoruba.Duende.IdentityServer.STS.Identity/wwwroot/js/signin-redirect.js
+++ src/Skoruba.Duende.IdentityServer.STS.Identity/wwwroot/js/signin-redirect.js
@@ -1,1 +1,25 @@
-window.location.href = document.querySelector("meta[http-equiv=refresh]").getAttribute("data-url");
+(function () {
+    var meta = document.querySelector("meta[http-equiv=refresh]");
+    var redirectUrl = meta && meta.getAttribute("data-url");
+
+    if (!redirectUrl) {
+        return;
+    }
+
+    var navigate = function () {
+        window.location.replace(redirectUrl);
+    };
+
+    var scheduleRedirect = function () {
+        window.requestAnimationFrame(function () {
+            window.setTimeout(navigate, 75);
+        });
+    };
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", scheduleRedirect, { once: true });
+        return;
+    }
+
+    scheduleRedirect();
+})();
